056362c74a52cfadf9442f229aeca2f9dcfe3e68
Ioana <cretuioana.dev@gmail.com>
2023-12-16 19:20:52 +0200

    created a random number generator

--- src/components/games/RandomNumber.js
+++ src/components/games/RandomNumber.js
@@ -1,3 +1,60 @@
+import { useState } from "react";
+
 export function RandomNumber() {
-  return <p>number</p>;
+  const [minValue, setMinValue] = useState(null);
+  const [maxValue, setMaxValue] = useState(null);
+  const [generatedNumber, setGeneratedNumber] = useState(null);
+
+  const handleGenerate = () => {
+    if (minValue !== null && maxValue !== null) {
+      const randomNumber = Math.floor(
+        Math.random() * (maxValue - minValue + 1) + minValue
+      );
+      setGeneratedNumber(randomNumber);
+    } else {
+      alert("Please enther both minimum and maximum values.");
+    }
+  };
+
+  const handleReset = () => {
+    setGeneratedNumber(null);
+    setMinValue(null);
+    setMaxValue(null);
+  };
+
+  return (
+    <div className="number-generator">
+      <h1>Random Number Generator</h1>
+      <p>
+        Keep in mind: The randomness is computer-generated, not truly random.
+      </p>
+      {generatedNumber ? (
+        <>
+          <div className="generated-number">{generatedNumber}</div>
+          <div className="buttons">
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleGenerate} style={{ marginLeft: "30px" }}>
+              Generate
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <input
+            type="number"
+            value={minValue}
+            placeholder="min"
+            onChange={(e) => setMinValue(Number(e.target.value))}
+          ></input>
+          <input
+            type="number"
+            value={maxValue}
+            placeholder="max"
+            onChange={(e) => setMaxValue(Number(e.target.value))}
+          ></input>
+          <button onClick={handleGenerate}>Generate</button>
+        </>
+      )}
+    </div>
+  );
 }
